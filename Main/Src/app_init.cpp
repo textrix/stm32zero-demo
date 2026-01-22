a76590b484b78a668b9f7d54258759236d0e416d
Textrix <t3xtrix@gmail.com>
2026-01-22 21:01:21 +0900

    refactor(app_init): use Priority enum from stm32zero-freertos
    Replace macro-based priority definitions with enum class.

--- Main/Src/app_init.cpp
+++ Main/Src/app_init.cpp
@@ -5,45 +5,10 @@
 #include "main.h"
 #include "cmsis_os.h"
 #include "stm32zero-debug.hpp"
+#include "stm32zero-freertos.hpp"
 #include <cstdio>
 
-/*
-    FreeRTOS 태스크 우선순위 정의
-
-    CMSIS-RTOS 버전에 따라 다른 우선순위 값을 사용한다.
-    v1과 v2의 호환성을 위해 통일된 인터페이스를 제공한다.
-
-    우선순위 레벨:
-        IDLE            유휴 태스크 (가장 낮음)
-        LOW             낮은 우선순위 작업
-        BELOWNORMAL     보통보다 낮음
-        NORMAL          기본 우선순위 (대부분 태스크)
-        ABOVENORMAL     보통보다 높음
-        HIGH            높은 우선순위 (중요 작업)
-        REALTIME        실시간 우선순위 (가장 높음)
-
-    참고:
-        - CMSIS v1: 0-6 범위
-        - CMSIS v2: 1-48 범위 (8단위 증가)
-        - 높은 숫자가 높은 우선순위
-*/
-#if (osCMSIS < 0x20000U)
-#define TASK_IDLE_PRIORITY          0
-#define TASK_LOW_PRIORITY           1
-#define TASK_BELOWNORMAL_PRIORITY   2
-#define TASK_NORMAL_PRIORITY        3
-#define TASK_ABOVENORMAL_PRIORITY   4
-#define TASK_HIGH_PRIORITY          5
-#define TASK_REALTIME_PRIORITY      6
-#else
-#define TASK_IDLE_PRIORITY          1   // osPriorityIdle
-#define TASK_LOW_PRIORITY           8   // osPriorityLow
-#define TASK_BELOWNORMAL_PRIORITY   16  // osPriorityBelowNormal
-#define TASK_NORMAL_PRIORITY        24  // osPriorityNormal
-#define TASK_ABOVENORMAL_PRIORITY   32  // osPriorityAboveNormal
-#define TASK_HIGH_PRIORITY          40  // osPriorityHigh
-#define TASK_REALTIME_PRIORITY      48  // osPriorityRealtime
-#endif
+using namespace stm32zero::freertos;
 
 static __NO_RETURN void SYSTEM_task_func_(void* arg)
 {
@@ -102,8 +67,8 @@ extern "C" __NO_RETURN void app_init(void)
 
 	// RTOS initialization and start (does not return)
 	osKernelInitialize();
-	xTaskCreate(INIT_task_func_, "INIT", 128, NULL, TASK_NORMAL_PRIORITY, NULL);
-	xTaskCreate(SYSTEM_task_func_, "SYSTEM", 256, NULL, TASK_NORMAL_PRIORITY, NULL);
+	xTaskCreate(INIT_task_func_, "INIT", 128, NULL, +Priority::NORMAL, NULL);
+	xTaskCreate(SYSTEM_task_func_, "SYSTEM", 256, NULL, +Priority::NORMAL, NULL);
 	osKernelStart();
 
 	// Should never reach here
