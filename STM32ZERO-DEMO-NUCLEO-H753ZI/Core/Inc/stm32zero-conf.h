3fbef9f4208eb8de85140ca20b5ebc759387e522
Textrix <t3xtrix@gmail.com>
2026-01-22 17:07:07 +0900

    Update demo for debug module refactoring
    - Add stm32zero-debug.cpp to CMakeLists.txt
    - Add STM32ZERO_DEBUG_UART to stm32zero-conf.h
    - Remove debug macros from test file
    - Update STM32ZERO submodule

--- STM32ZERO-DEMO-NUCLEO-H753ZI/Core/Inc/stm32zero-conf.h
+++ STM32ZERO-DEMO-NUCLEO-H753ZI/Core/Inc/stm32zero-conf.h
@@ -8,6 +8,12 @@
 // STM32H7 D-Cache line size
 #define STM32ZERO_CACHE_LINE_SIZE  32
 
+// Debug UART (NUCLEO-H753ZI uses USART3 via ST-Link VCP)
+#define STM32ZERO_DEBUG_UART  huart3
+
+// Debug buffer size (4KB x 2 = 8KB total)
+#define STM32ZERO_DEBUG_BUFFER_SIZE  4096
+
 // FreeRTOS enabled
 #define STM32ZERO_RTOS_FREERTOS    1
 
