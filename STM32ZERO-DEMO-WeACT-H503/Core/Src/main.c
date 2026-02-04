c9204f7286ce70e77fda21da53014ed90cf5e6ff
Textrix <t3xtrix@gmail.com>
2026-02-04 15:12:03 +0900

    chore(WeACT-H503): update CubeMX config
    - Change LSE drive to HIGH for better oscillator startup
    - Remove UART1 TX/RX swap (not needed for this board)
    - Remove unused IAR portable files from FreeRTOS
    - Update toolchain setting to STM32CubeIDE

--- STM32ZERO-DEMO-WeACT-H503/Core/Src/main.c
+++ STM32ZERO-DEMO-WeACT-H503/Core/Src/main.c
@@ -193,7 +193,7 @@ void SystemClock_Config(void)
   *  Warning : Only applied when the LSE is disabled.
   */
   HAL_PWR_EnableBkUpAccess();
-  __HAL_RCC_LSEDRIVE_CONFIG(RCC_LSEDRIVE_LOW);
+  __HAL_RCC_LSEDRIVE_CONFIG(RCC_LSEDRIVE_HIGH);
 
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
@@ -950,8 +950,7 @@ static void MX_USART1_UART_Init(void)
   huart1.Init.OverSampling = UART_OVERSAMPLING_16;
   huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
   huart1.Init.ClockPrescaler = UART_PRESCALER_DIV1;
-  huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_SWAP_INIT;
-  huart1.AdvancedInit.Swap = UART_ADVFEATURE_SWAP_ENABLE;
+  huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
   if (HAL_UART_Init(&huart1) != HAL_OK)
   {
     Error_Handler();
